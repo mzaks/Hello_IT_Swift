460470de821491b780ca2ff48b09f1e76b73b97a
Maxim Zaks <maxim.zaks@gmail.com>
2016-10-21 15:56:04 +0200

    Initial Commit

---
+++ Hello_IT_Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello_IT_Swift
+//
+//  Created by Maxim Zaks on 21.10.16.
+//  Copyright © 2016 Maxim Zaks. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
